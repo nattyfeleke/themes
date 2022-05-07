8737a929fcedef233b51ce9010c8f16adc7a9b2c
Natnael Feleke <nfeleke568@gmail.com>
2022-05-07 08:09:23 +0300

    setting up new theme

--- my-theme/lms/static/javascript/carousel.js
+++ my-theme/lms/static/javascript/carousel.js
@@ -1,44 +1,44 @@
-// const track = document.querySelector('.carousel__track');
-// const slides = Array.from(track.children);
+const track = document.querySelector('.carousel__track');
+const slides = Array.from(track.children);
 
-// const dotsNav = document.querySelector('.carousel__nav');
-// const dots = Array.from(dotsNav.children);
+const dotsNav = document.querySelector('.carousel__nav');
+const dots = Array.from(dotsNav.children);
 
-// const slideWidth = slides[0].getBoundingClientRect().width;
-// //arrange the slides next to one another
-// const setSledePosition = (slide, index)=> {
-//     slide.style.left = slideWidth * index + 'px';
-// }
-// slides.forEach(setSledePosition);
-
-
-// const moveToSlide = (track, currentSlide, targetSlide) => {
-//     track.style.transform = `translateX(-${targetSlide.style.left})`
-//     currentSlide.classList.remove('current-slide')
-//     targetSlide.classList.add('current-slide')
-// }
-// const updateDots = (currentDot, targetDot) => {
-//     currentDot.classList.remove('current-slide')
-// targetDot.classList.add('current-slide')
-// }
+const slideWidth = slides[0].getBoundingClientRect().width;
+//arrange the slides next to one another
+const setSledePosition = (slide, index)=> {
+    slide.style.left = slideWidth * index + 'px';
+}
+slides.forEach(setSledePosition);
 
 
-// //When I click the nav indicators move to that slide
-// dotsNav.addEventListener('click', e=> {
-//     //what indicator is clicked on?
-//     const targetDot = e.target.closest('button') ;
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = `translateX(-${targetSlide.style.left})`
+    currentSlide.classList.remove('current-slide')
+    targetSlide.classList.add('current-slide')
+}
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('current-slide')
+targetDot.classList.add('current-slide')
+}
 
-// if(!targetDot) return;
 
-// const currentSlide = track.querySelector('.current-slide');
-// const currentDot = dotsNav.querySelector('.current-slide')
-// const targetIndex = dots.findIndex(dot => dot === targetDot)
-// const targetSlide = slides[targetIndex];
-// moveToSlide(track, currentSlide, targetSlide)
+//When I click the nav indicators move to that slide
+dotsNav.addEventListener('click', e=> {
+    //what indicator is clicked on?
+    const targetDot = e.target.closest('button') ;
 
-// updateDots(currentDot, targetDot);
+if(!targetDot) return;
 
-// })
+const currentSlide = track.querySelector('.current-slide');
+const currentDot = dotsNav.querySelector('.current-slide')
+const targetIndex = dots.findIndex(dot => dot === targetDot)
+const targetSlide = slides[targetIndex];
+moveToSlide(track, currentSlide, targetSlide)
+
+updateDots(currentDot, targetDot);
+
+})
 
 
 //     window.setInterval(function() {
