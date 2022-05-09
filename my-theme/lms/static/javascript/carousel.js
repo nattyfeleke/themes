3a6bc67e74e749ed27cf50527c18571263fb2c1b
Natnael Feleke <nfeleke568@gmail.com>
2022-05-09 18:17:37 +0300

    setting up new theme

--- my-theme/lms/static/javascript/carousel.js
+++ my-theme/lms/static/javascript/carousel.js
@@ -41,22 +41,22 @@ updateDots(currentDot, targetDot);
 })
 
 
-//     window.setInterval(function() {
-//         const firstSlide = slides[0];
+    window.setInterval(function() {
+        const firstSlide = slides[0];
        
-//         const targetDot = dotsNav.querySelector('.current-slide');
-//      const targetIndex = dots.findIndex(dot => dot ===targetDot)
-//          const currentSlide = track.querySelector('.current-slide');
-//          const nextSlide = targetIndex === slides.length -1 ? firstSlide : currentSlide.nextElementSibling;
+        const targetDot = dotsNav.querySelector('.current-slide');
+     const targetIndex = dots.findIndex(dot => dot ===targetDot)
+         const currentSlide = track.querySelector('.current-slide');
+         const nextSlide = targetIndex === slides.length -1 ? firstSlide : currentSlide.nextElementSibling;
      
-//          moveToSlide(track, currentSlide, nextSlide);
+         moveToSlide(track, currentSlide, nextSlide);
      
-//          const firstDot = dots[0];
-//          const currentDot = dotsNav.querySelector('.current-slide')
-//      const nextDot = targetIndex === slides.length -1 ? firstDot : currentDot.nextElementSibling;
+         const firstDot = dots[0];
+         const currentDot = dotsNav.querySelector('.current-slide')
+     const nextDot = targetIndex === slides.length -1 ? firstDot : currentDot.nextElementSibling;
      
-//      updateDots(currentDot, nextDot)
+     updateDots(currentDot, nextDot)
      
      
      
-//        }, 5000);
+       }, 5000);
